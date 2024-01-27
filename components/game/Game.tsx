ca94b71a4686db388d3f66b1ef71b9d83c5e1742
Visa Pollari <visa.pollari@gmail.com>
2024-01-27 13:27:37 +0200

    hok

--- components/game/Game.tsx
+++ components/game/Game.tsx
@@ -1,11 +1,28 @@
 'use client';
 import { Map } from './map/Map';
 import { Stage, Container } from '@pixi/react';
+import { useEffect, useState } from 'react';
 export const Game = () => {
+  const [gameSize, setGameSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setGameSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <Stage
-      width={window?.innerWidth || 0}
-      height={window?.innerHeight || 0}
+      width={gameSize.width}
+      height={gameSize.height}
       options={{ backgroundColor: 0x101010 }}
     >
       <Container x={400} y={230}>
